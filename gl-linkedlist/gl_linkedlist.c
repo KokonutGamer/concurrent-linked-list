6b2a23146955fa8286fc8b11154f0ad126a8017f
Gabe Lapingcao <mgultra2018@gmail.com>
2026-03-08 14:20:01 -0700

    Implemented insertions for global-lock

--- gl-linkedlist/gl_linkedlist.c
+++ gl-linkedlist/gl_linkedlist.c
@@ -28,13 +28,82 @@ void destroyList(GLLinkedList *list) {
   free(list);
 }
 
-void append(GLLinkedList *list, int data);
+void append(GLLinkedList *list, int data) {
+  // exit early if the list is null
+  if (list == NULL) {
+    return;
+  }
 
-void insertNode(GLLinkedList *list, int index, int data);
+  // acquire the lock on the list first
+  pthread_mutex_lock(&list->lock);
 
-void insertHead(GLLinkedList *list, int data);
+  // immediately append the node if the head is null
+  if (list->head == NULL) {
+    list->head = createNode(data);
+    pthread_mutex_unlock(&list->lock);
+    return;
+  }
 
-void insertTail(GLLinkedList *list, int data);
+  // traverse the list starting at the head
+  GLNode *curr = list->head;
+
+  while (curr->next != NULL) {
+    curr = curr->next;
+  }
+
+  // append the new node to the end of the list
+  curr->next = createNode(data);
+  pthread_mutex_unlock(&list->lock);
+}
+
+void insertNode(GLLinkedList *list, int index, int data) {
+  // exit early if the list is null
+  if (list == NULL) {
+    return;
+  }
+
+  // acquire the lock on the list first
+  pthread_mutex_lock(&list->lock);
+
+  if (index == 0) {
+    // insert the node as the new head of the list
+    GLNode *next = list->head;
+    list->head = createNode(data);
+    list->head->next = next;
+    pthread_mutex_unlock(&list->lock);
+    return;
+  }
+
+  // check if the head is null before traversing the list; index out of bounds
+  if (list->head == NULL) {
+    pthread_mutex_unlock(&list->lock);
+    return;
+  }
+
+  // decrement the index; the neighbor of the current is the target index
+  index--;
+  GLNode *curr = list->head;
+  while (curr->next != NULL && index != 0) {
+    curr = curr->next;
+    index--;
+  }
+
+  if (index != 0) {
+    // relinquish the lock on the list; index out of bounds
+    pthread_mutex_unlock(&list->lock);
+    return;
+  }
+
+  // insert the new node now that we've found where to insert at
+  GLNode *next = curr->next;
+  curr->next = createNode(data);
+  curr->next->next = next;
+  pthread_mutex_unlock(&list->lock);
+}
+
+void insertHead(GLLinkedList *list, int data) { insertNode(list, 0, data); }
+
+void insertTail(GLLinkedList *list, int data) { append(list, data); }
 
 int size(GLLinkedList *list);
 
