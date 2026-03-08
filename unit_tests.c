33131a1b6f4ec0c704ee3d31b58211904f5e4cea
Gabe Lapingcao <mgultra2018@gmail.com>
2026-03-07 23:52:16 -0800

    Implemented out-of-bounds checks

--- unit_tests.c
+++ unit_tests.c
@@ -19,9 +19,22 @@ static void assertEquals(int actual, int expected, const char *name) {
 }
 
 /**
- *
+ * Internal function used for negative comparison assertions.
  */
-static int runTest(void (*testFunction)(HOHLinkedList *), HOHLinkedList *list) {
+static void assertNotEquals(int actual, int notExpected, const char *name) {
+  if (actual != notExpected) {
+    printf("[PASSED] %s\n", name);
+  } else {
+    printf("[FAILED] %s, %d was not expected\n", name, notExpected);
+  }
+}
+
+/**
+ * Internal function used for testing functions with a supplied list returning
+ * void.
+ */
+static int runWithList(void (*testFunction)(HOHLinkedList *),
+                       HOHLinkedList *list) {
   // fork the process at this execution point
   pid_t pid = fork();
 
@@ -48,11 +61,57 @@ static int runTest(void (*testFunction)(HOHLinkedList *), HOHLinkedList *list) {
 }
 
 /**
- * Internal function used for segfault assertions.
+ * Internal function used for testing functions with a supplied list and value
+ * returning void.
  */
-static void assertNoSegFault(void (*testFunction)(HOHLinkedList *),
-                             HOHLinkedList *list, const char *name) {
-  if (runTest(testFunction, list)) {
+static int runWithListValue(void (*testFunction)(HOHLinkedList *, int),
+                            HOHLinkedList *list, int value) {
+  // fork the process at this execution point
+  pid_t pid = fork();
+
+  if (pid == -1) {
+    perror("Fork failed");
+    exit(EXIT_FAILURE);
+  } else if (pid == 0) {
+    // within child process
+    testFunction(list, value);
+    exit(EXIT_SUCCESS);
+  } else {
+    // within parent process; check on the status of the child
+    int status;
+    waitpid(pid, &status, 0);
+
+    if (WIFSIGNALED(status)) {
+      // detect segmentation faults
+      if (WTERMSIG(status) == SIGSEGV) {
+        return 0;
+      }
+    }
+    return 1; // no segmentation fault occurred
+  }
+}
+
+/**
+ * Internal function used for segfault assertions on functions with a supplied
+ * list returning void.
+ */
+static void assertNoSegFaultList(void (*testFunction)(HOHLinkedList *),
+                                 HOHLinkedList *list, const char *name) {
+  if (runWithList(testFunction, list)) {
+    printf("[PASSED] %s\n", name);
+  } else {
+    printf("[FAILED] %s, segmentation fault detected\n", name);
+  }
+}
+
+/**
+ * Internal function used for segfault assertions on functions with a supplied
+ * list and value returning void.
+ */
+static void
+assertNoSegFaultListValue(void (*testFunction)(HOHLinkedList *, int value),
+                          HOHLinkedList *list, int value, const char *name) {
+  if (runWithListValue(testFunction, list, value)) {
     printf("[PASSED] %s\n", name);
   } else {
     printf("[FAILED] %s, segmentation fault detected\n", name);
@@ -75,14 +134,18 @@ void emptyListTests() {
   assertEquals(back(list), 0, "Empty list tail is zero");
   assertEquals(get(list, 0), 0, "Empty list at index 0 is zero");
 
-  assertNoSegFault(removeHead, list,
-                   "Empty list head removal results in no segmentation fault");
-  assertNoSegFault(removeTail, list,
-                   "Empty list tail removal results in no segmentation fault");
-  assertNoSegFault(clearList, list,
-                   "Clearing an empty list results in no segmentation fault");
-  assertNoSegFault(printList, list,
-                   "Printing an empty list results in no segmentation fault");
+  assertNoSegFaultList(
+      removeHead, list,
+      "Empty list head removal results in no segmentation fault");
+  assertNoSegFaultList(
+      removeTail, list,
+      "Empty list tail removal results in no segmentation fault");
+  assertNoSegFaultList(
+      clearList, list,
+      "Clearing an empty list results in no segmentation fault");
+  assertNoSegFaultList(
+      printList, list,
+      "Printing an empty list results in no segmentation fault");
 }
 
 void singleElementListTests() {
@@ -97,7 +160,7 @@ void singleElementListTests() {
   // assert
   assertEquals(front(list), value, "Single-element list head is a constant");
   assertEquals(back(list), value, "Single-element list tail is a constant");
-  assertNoSegFault(
+  assertNoSegFaultList(
       printList, list,
       "Printing a single-element list results in no segmentation fault");
 
@@ -111,11 +174,40 @@ void singleElementListTests() {
   assertEquals(
       back(list), 0,
       "Single-element list tail removal results in tail equal to zero");
-  assertNoSegFault(printList, list,
-                   "Printing an empty list results in no segmentation fault");
+  assertNoSegFaultList(
+      printList, list,
+      "Printing an empty list results in no segmentation fault");
+}
+
+void outOfBoundsListTests() {
+  // arrange
+  HOHLinkedList *list;
+  initList(&list);
+  int values[] = {23, 59, 47};
+
+  // act
+  for (int i = 0; i < 3; i++) {
+    append(list, values[i]);
+  }
+
+  // assert
+  assertEquals(get(list, 999), 0,
+               "Attempting to retrieve an out-of-bounds index returns 0");
+  assertNoSegFaultListValue(removeNode, list, 999,
+                            "Attempting to remove an out-of-bounds index "
+                            "results in no segmentation fault");
+
+  // act
+  insertNode(list, 999, 5);
+
+  // assert
+  assertNotEquals(back(list), 5,
+                  "Attempting to insert an out-of-bounds index results in no "
+                  "new insertion");
 }
 
 int main() {
   emptyListTests();
   singleElementListTests();
+  outOfBoundsListTests();
 }
